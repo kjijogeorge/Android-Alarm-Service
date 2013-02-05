834d286090ae8b028f103b6370ec0e2606f87a42
kjijogeorge <jijogeorge277@gmail.com>
2013-02-05 10:05:58 +0530

    Initial commit

---
+++ src/com/exercise/AndroidAlarmService/AndroidAlarmService.java
@@ -0,0 +1,52 @@
+package com.exercise.AndroidAlarmService;
+
+import java.util.Calendar;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class AndroidAlarmService extends Activity {
+
+private PendingIntent pendingIntent;
+private Intent myIntent;
+
+ /** Called when the activity is first created. */
+ @Override
+ public void onCreate(Bundle savedInstanceState) {
+     super.onCreate(savedInstanceState);
+     setContentView(R.layout.main);
+     Button buttonStart = (Button)findViewById(R.id.startalarm);
+     Button buttonCancel = (Button)findViewById(R.id.cancelalarm);
+  
+     buttonStart.setOnClickListener(new Button.OnClickListener(){
+
+	  @Override
+	  public void onClick(View arg0) {
+		   myIntent = new Intent(AndroidAlarmService.this, MyAlarmService.class);
+		   pendingIntent = PendingIntent.getService(AndroidAlarmService.this, 0, myIntent, 0);
+		   AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
+		   Calendar calendar = Calendar.getInstance();
+		   calendar.setTimeInMillis(System.currentTimeMillis());
+		   calendar.add(Calendar.SECOND, 5);
+		   alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+		   Toast.makeText(AndroidAlarmService.this, "Start Alarm", Toast.LENGTH_LONG).show();
+	  }});
+  
+     buttonCancel.setOnClickListener(new Button.OnClickListener(){
+
+	  @Override
+	  public void onClick(View arg0) {
+		    AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
+			alarmManager.cancel(pendingIntent);
+			stopService(myIntent);
+		    Toast.makeText(AndroidAlarmService.this, "Cancel!", Toast.LENGTH_LONG).show();
+	  }});
+  
+ }
+}
